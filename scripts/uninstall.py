2da35e1b65e6dfa9401d65aee6aef501736b93d1
andrzejs-gh <249625581+andrzejs-gh@users.noreply.github.com>
2026-01-28 21:40:42 +0100

    refactor uninstall.py

--- scripts/uninstall.py
+++ scripts/uninstall.py
@@ -1,22 +1,21 @@
 import sys, os
 from utils import *
 
-def delete_files(paths):
-	for path in paths:
-		if os.path.isfile(path):
-			try:
-				os.remove(path)
-				print(f"Removed {path} {ok}")
-			except Exception as e:
-				print(f"Could not remove: {path} {fail}:\n{e}")
-		else:
-			print(f"No {os.path.basename(path)} found in {os.path.dirname(path)} {fail}")
+def delete_cogp(path):
+	if os.path.isfile(path):
+		try:
+			os.remove(path)
+			print(f"Removed {path} {ok}")
+		except Exception as e:
+			print(f"Could not remove: {path} {fail}:\n{e}")
+	else:
+		print(f"No cogp found in {os.path.dirname(path)} {fail}")
 
 if __name__ == "__main__":
 	try:
 		home_dir = get_home_dir()
 		cogp_path = home_dir + "/.local/bin/cogp"
-		delete_files([cogp_path])
+		delete_cogp(cogp_path)
 		
 		print(green("Uninstallation completed.\n"))
 		input("Press ENTER to exit.")
